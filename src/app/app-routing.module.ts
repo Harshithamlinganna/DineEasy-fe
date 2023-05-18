eac9951aa8866ba81cbd3976d1d489d90b8a5ae1
kapish-patel <kpatelkapish@gmail.com>
2023-05-18 05:20:18 -0700

    added order model

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurant/restaurant.component';
 import { RestaurantOverviewComponent } from './restaurant-overview/restaurant-overview.component';
+import { OrderComponent } from './order/order.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
@@ -11,7 +12,8 @@ const routes: Routes = [
   { path: 'restaurants', component: RestaurantsComponent},
   { path: 'restaurants/:id', component: RestaurantComponent, children: [
     {path: '', redirectTo: '/overview', pathMatch: 'full' },
-    { path: 'overview', component: RestaurantOverviewComponent }
+    { path: 'overview', component: RestaurantOverviewComponent },
+    { path: 'order', component: OrderComponent }
 ]}];
 
 @NgModule({
